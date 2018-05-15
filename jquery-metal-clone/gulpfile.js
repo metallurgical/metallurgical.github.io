d2a744915cb96c2b30f12fe620045e7d2e6e22b6
Norlihazmey Ghazali <norlihazmey.ghazali@gmail.com>
2018-05-15 13:02:58 +0800

    adding new documenatation

--- jquery-metal-clone/gulpfile.js
+++ jquery-metal-clone/gulpfile.js
@@ -1,67 +1,125 @@
-
-// define all gulp plugin to used
-// define plugin if required
-// some of plugins can be used together with others plugin
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
 
-//this plugin for debug js file, will display error and correction into console
-gulp.task('jshint', function(){
+// Set the banner content
+var banner = ['/*!\n',
+  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' */\n',
+  ''
+].join('');
 
-	var srcJs = './jquery.metalClone.js'; // js source files
-	gulp.src(srcJs)
-	    .pipe(jshint())
-	    .pipe(jshint.reporter('default'));
+// Compiles SCSS files from /scss into /css
+gulp.task('sass', function() {
+  return gulp.src('scss/resume.scss')
+    .pipe(sass())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-
-
-
-
-//this function to concat(can be used to concat css file also) all js file and code, strip empty space js file
-//This example passes an array of filenames to gulp.src(); I want scripts.js to appear at the top of the production file followed by all other JavaScript files in any order. 
-gulp.task('myJs', function(){
-
-	gulp.src(['./jquery.metalClone.js'])
-		.pipe(concat('jquery.metalClone.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('./'));
+// Minify compiled CSS
+gulp.task('minify-css', ['sass'], function() {
+  return gulp.src('css/resume.css')
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-
-
-
-//to run all the task sekaligus(sequence) guna code ni
-//just run 'gulp' command on console to run the default task
-gulp.task('default', ['jshint','myJs'], function(){
-
-
-
-/********************************** optional buang kod dalm block ni shaja klu xse(to watch the changes realtime use this)**********/
-
-	// watch for HTML changes
-  gulp.watch('./src/*.html', function() {
-    gulp.run('minifyHtml');
-  });
- 
-  // watch for JS changes
-  gulp.watch('./src/scripts/*.js', function() {
-    gulp.run('jshint', 'scripts');
-  });
- 
-  // watch for CSS changes
-  gulp.watch('./src/styles/*.css', function() {
-    gulp.run('css');
-  });
-
- /***************************** end optional **************************/
-
+// Minify custom JS
+gulp.task('minify-js', function() {
+  return gulp.src('js/resume.js')
+    .pipe(uglify())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('js'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
- 
 
+// Copy vendor files from /node_modules into /vendor
+// NOTE: requires `npm install` before running!
+gulp.task('copy', function() {
+  gulp.src([
+      'node_modules/bootstrap/dist/**/*',
+      '!**/npm.js',
+      '!**/bootstrap-theme.*',
+      '!**/*.map'
+    ])
+    .pipe(gulp.dest('vendor/bootstrap'))
 
+  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest('vendor/jquery'))
 
+  gulp.src(['node_modules/jquery.easing/*.js'])
+    .pipe(gulp.dest('vendor/jquery-easing'))
 
+  gulp.src([
+      'node_modules/font-awesome/**',
+      '!node_modules/font-awesome/**/*.map',
+      '!node_modules/font-awesome/.npmignore',
+      '!node_modules/font-awesome/*.txt',
+      '!node_modules/font-awesome/*.md',
+      '!node_modules/font-awesome/*.json'
+    ])
+    .pipe(gulp.dest('vendor/font-awesome'))
 
+  gulp.src([
+      'node_modules/devicons/**/*',
+      '!node_modules/devicons/*.json',
+      '!node_modules/devicons/*.md',
+      '!node_modules/devicons/!PNG',
+      '!node_modules/devicons/!PNG/**/*',
+      '!node_modules/devicons/!SVG',
+      '!node_modules/devicons/!SVG/**/*'
+    ])
+    .pipe(gulp.dest('vendor/devicons'))
+
+  gulp.src(['node_modules/simple-line-icons/**/*', '!node_modules/simple-line-icons/*.json', '!node_modules/simple-line-icons/*.md'])
+    .pipe(gulp.dest('vendor/simple-line-icons'))
+})
+
+// Default task
+gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('css/*.css', ['minify-css']);
+  gulp.watch('js/*.js', ['minify-js']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('js/**/*.js', browserSync.reload);
+});
